66a3486daae7c3d8baae082e649583541b082514
unknown <muritalaadebukola01@gmail.com>
2024-02-21 20:16:37 +0100

    datatypes

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,45 @@
+/*let js = 'amazing';
+      console.log(40 + 8 + 23 -10);
+      console.log("jonas");
+      console.log(23); 
+
+      let firstName = "matilda"; 
+      console.log(firstName);
+      console.log(firstName);
+      console.log(firstName); 
+      
+      let jonas_matilda = "JM";
+      let $function = 27;
+
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = "coder";
+let myCurrentJob = "teacher"; 
+
+let job1 = "programmer";
+let job2 = "teacher"; 
+
+console.log(myFirstJob);
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+//console.log(typeof true);
+console.log(typeof javascriptIsFun);
+//console.log(typeof 23);
+//console.log(typeof 'jonas'); 
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof  year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);*/
+let age = 30;
+age = 31;
